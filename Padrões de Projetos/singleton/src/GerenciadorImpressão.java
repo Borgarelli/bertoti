dcb2f52bebb25af40cb18bb2a558a7e90eccd336
Borgarelli <borgarellitavares@outlook.com>
2022-11-17 16:38:19 -0300

    singleton fix

--- Padrões de Projetos/singleton/src/GerenciadorImpressão.java
+++ Padrões de Projetos/singleton/src/GerenciadorImpressão.java
@@ -1,12 +1,13 @@
 public class GerenciadorImpressão {
+
     private static GerenciadorImpressão gerenciadorImpressão;
 
-    private GerenciadorImpressão(){
+    private GerenciadorImpressão(){ //criação do metódo construtor sem parametros
 
     }
 
     public static GerenciadorImpressão getInstance(){
-        if (gerenciadorImpressão == null){
+        if (gerenciadorImpressão == null){ //Aqui entra a lógica do Singleton, porque sempre que o valor foi igual a nulo, ele criara um novo espaço alocado na memória, porém ele nunca é nulo, por ja ocupar um espaço ele nunca vai retornar outro valor na memória
         gerenciadorImpressão = new GerenciadorImpressão();
     }
         return gerenciadorImpressão;
