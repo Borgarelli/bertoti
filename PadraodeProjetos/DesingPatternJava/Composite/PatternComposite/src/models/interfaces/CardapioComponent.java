fb5371fa3408ec74d4a127e22da9fb00b3f69d03
Kauã Borgarelli <79945984+Borgarelli@users.noreply.github.com>
2023-05-15 19:08:48 -0300

    new patterns and antpatterns

---
+++ PadraodeProjetos/DesingPatternJava/Composite/PatternComposite/src/models/interfaces/CardapioComponent.java
@@ -0,0 +1,9 @@
+package models.interfaces;
+
+public interface CardapioComponent {
+	
+	public String getNome();
+	public void adcionar(CardapioComponent lanche);
+	public void remover(CardapioComponent lanche);
+	
+}
